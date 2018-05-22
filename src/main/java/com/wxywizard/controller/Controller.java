76eb2257df80f42b8a1a3ba119f09febddb88edb
wxywizard <wxy12125@vip.qq.com>
2018-05-22 18:00:42 +0800

    引入lombok  配置logback配置文件

--- src/main/java/com/wxywizard/controller/Controller.java
+++ src/main/java/com/wxywizard/controller/Controller.java
@@ -1,13 +1,17 @@
 package com.wxywizard.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class Controller {
 
     @RequestMapping(value = "/test{id}",method = RequestMethod.GET)
     public String test(@PathVariable(value = "id",required = true) String id){
 
+        log.info("id==={}",id);
         return id;
     }
 }
